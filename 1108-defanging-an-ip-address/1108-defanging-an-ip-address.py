76a2d4a5658353d52c34a1b2ae2a42b6d726a388
Ranabothu Keerthika <keerthikareddy2325@gmail.com>
2026-05-15 21:26:43 +0530

    Time: 43 ms (66.77%), Space: 19.2 MB (38.84%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -0,0 +1,8 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".","[.]")
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
