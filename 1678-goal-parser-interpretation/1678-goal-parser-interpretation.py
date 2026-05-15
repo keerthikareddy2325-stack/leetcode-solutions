4299c3da9d15df52a1ee9b152757608c84ac6712
Ranabothu Keerthika <keerthikareddy2325@gmail.com>
2026-05-15 22:59:05 +0530

    Time: 54 ms (7.04%), Space: 19.2 MB (82.41%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        return command.replace("()","o").replace("(al)","al")
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
