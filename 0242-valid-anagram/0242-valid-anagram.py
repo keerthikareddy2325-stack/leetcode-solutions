668302186e5b571019cae2bcc142edbbf75631be
Ranabothu Keerthika <keerthikareddy2325@gmail.com>
2026-06-11 22:02:22 +0530

    Time: 19 ms (15.52%), Space: 20.8 MB (9.16%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1=sorted(list(s))
+        l2=sorted(list(t))
+        if l1==l2:
+            return True
+        else:
+            return False
