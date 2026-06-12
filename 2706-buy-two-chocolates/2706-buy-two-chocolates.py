f5133302cee4189e24c181b0864e8671a68c1eab
Ranabothu Keerthika <keerthikareddy2325@gmail.com>
2026-06-12 15:28:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (17.9%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        s=prices[0]+prices[1]
+        if s<=money:
+            return money-s
+        else:
+            return money
