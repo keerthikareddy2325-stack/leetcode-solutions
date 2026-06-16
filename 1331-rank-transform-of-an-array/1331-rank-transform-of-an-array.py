0ca0aebe74b495c75e35c77e8bf50332606c1aba
Ranabothu Keerthika <keerthikareddy2325@gmail.com>
2026-06-16 22:56:34 +0530

    Time: 36 ms (62.75%), Space: 37.4 MB (82.1%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        s = sorted(set(arr))
-        d = {}
+        s=sorted(set(arr))
+        d={}
         for i in range(len(s)):
-            d[s[i]] = i + 1
+            d[s[i]]=i + 1
         return [d[x] for x in arr]
